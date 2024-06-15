2c33e92580a3760fbf4bee169e24f41985015471
lyonsyonii <liam@garriga.dev>
2024-06-15 12:34:19 +0200

    update Astro base

--- example/astro.config.mjs
+++ example/astro.config.mjs
@@ -5,7 +5,7 @@ import compressor from "astro-compressor";
 
 // https://astro.build/config
 export default defineConfig({
-  base: "rubri",
+  base: "/rubri",
   integrations: [compress(), compressor({
     brotli: false,
     fileExtensions: [".css", ".js", ".html", ".xml", ".cjs", ".mjs", ".svg", ".txt"]
